a7c57e1c316037792887f34499f1f5fe92a48535
Matt Antone <matt.antone@gmai.com>
2019-10-06 10:14:53 -0700

    added shortcodes & styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ const { src, dest } = require('gulp');
 const ext_replace = require('gulp-ext-replace');
 var flatten = require('gulp-flatten');
 
-function copyFontAwesome() {
+function copyAsTemplates() {
   return src('node_modules/@fortawesome/**/svgs/**/*.svg')
     .pipe(flatten())
     .pipe(ext_replace('.html'))
     .pipe(dest('layouts/partials/icons/'));
 }
 
-exports.installFontAwesome = copyFontAwesome;
+function copyAsSVG(){
+  return src('node_modules/@fortawesome/**/svgs/**/*.svg')
+    .pipe(flatten())
+    .pipe(ext_replace('.html'))
+    .pipe(dest('static/icons/'));
+}
+
+exports.copyAsTemplates = copyAsTemplates;
+exports.copySVG = copyAsSVG;
